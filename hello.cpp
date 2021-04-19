666345bbe0bd0cf4b22b651e449715eeef989573
goldv <843951713@qq.com>
2021-04-19 21:15:40 +0800

    hello.cpp init

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 19 Apr 2021 09:10:14 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "helloworld!" << endl;
+    return 0;
+}
